884a39ca13eaee656b8604bcbf9ce2c23fa75791
f1rul4yx <vargasgomezderequenadiego@gmail.com>
2024-12-14 16:36:44 +0100

    Update update.sh 1

--- configurations/scripts/update/update.sh
+++ configurations/scripts/update/update.sh
@@ -1,8 +1,28 @@
 #!/bin/zsh
 
 clear
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt autopurge -y
-sudo apt autoremove -y
-sudo apt autoclean -y
+apt list --upgradable
+echo -en "\n¿Quieres actualizar los paquetes? (s/n): "
+read respuesta_actualizar
+case "$respuesta_actualizar" in
+        [Ss]*)
+                sudo apt update -y
+                sudo apt upgrade -y
+                ;;
+        *)
+                echo -e "\nNo se actualizaron los paquetes."
+                ;;
+esac
+echo -en "\n¿Quieres limpiar paquetes obsoletos? (s/n): "
+read respuesta_limpiar
+case "$respuesta_limpiar" in
+        [Ss]*)
+                sudo apt autopurge -y
+                sudo apt autoremove -y
+                sudo apt autoclean -y
+                ;;
+        *)
+                echo -e "\nNo se eliminaron los paquetes obsoletos."
+                ;;
+esac
+echo -e "\nOperaciones completadas."
